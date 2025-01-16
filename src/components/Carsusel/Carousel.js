390939907d569eb061697a6d73d982eab355264b
Rafiul Islam <www.rafiulislam1998@gmail.com>
2025-01-16 15:11:08 +0600

    added style on carousel

--- src/components/Carsusel/Carousel.js
+++ src/components/Carsusel/Carousel.js
@@ -29,28 +29,30 @@ const Carousel = () => {
   };
 
   return (
-    <div className="text-center mb-12">
-      <Image
-        src={images[currentIndex]} 
-        alt="Gold Jewelry Collection"
-        width={800}
-        height={300}
-        className="rounded-lg shadow-lg"
-      />
-      <div className="mt-4 flex justify-center space-x-4">
+    <div className="relative text-center mb-12">
+      <div className="absolute top-1/2 left-0 right-0 flex justify-between px-4 py-2 z-10">
         <button
           onClick={prevImage}
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+          className="bg-gray-100 text-amber-700 px-4 py-2 rounded-lg hover:bg-blue-700"
         >
           Prev
         </button>
         <button
           onClick={nextImage}
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+          className="bg-gray-100 text-amber-700 px-4 py-2 rounded-lg hover:bg-blue-700"
         >
           Next
         </button>
       </div>
+      
+      {/* Image */}
+      <Image
+        src={images[currentIndex]} 
+        alt="Gold Jewelry Collection"
+        width={1350}
+        height={50}
+        className="w-full  rounded-lg shadow-lg" 
+      />
     </div>
   );
 };
